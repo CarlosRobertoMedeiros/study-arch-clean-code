c2b7bbccb4e5f7b49e550ed64f5f5f7a49505ab1
Carlos Roberto <carlosmedeiroslima@gmail.com>
2022-06-16 23:59:24 -0300

    Implementando o Teste

--- src/test/java/br/com/roberto/codigoruim/refatoracaocomplexidadecognitiva/SorveteriaTest.java
+++ src/test/java/br/com/roberto/codigoruim/refatoracaocomplexidadecognitiva/SorveteriaTest.java
@@ -1,15 +1,42 @@
 package br.com.roberto.codigoruim.refatoracaocomplexidadecognitiva;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SorveteriaTest {
 
+    private Sorveteria sorveteria;
+    @BeforeEach
+    void setup(){
+        sorveteria = new Sorveteria();
+    }
+
     @Test
-    void precoSorvete() {
-        Sorveteria sorveteria = new Sorveteria();
-        int valorRetorno =  sorveteria.precoSorvete(true,true,1);
-        System.out.println("valorRetorno:" +valorRetorno);
+    void validaSorveteSimplesSemCasquinhaSemCobertura() {
+        Assertions.assertEquals(17,sorveteria.precoSorvete(false,false,0));
+    }
+
+    @Test
+    void validaSorvetePremiumSemCasquinhaSemCobertura() {
+        Assertions.assertEquals(21,sorveteria.precoSorvete(true,false,0));
+    }
+
+    @Test
+    void validaSorvetePremiumComCasquinhaSemCobertura() {
+        Assertions.assertEquals(23,sorveteria.precoSorvete(true,true,0));
+    }
+
+    @Test
+    void validaSorvetePremiumComCasquinhaComUmaCobertura() {
+        Assertions.assertEquals(23,sorveteria.precoSorvete(true,true,1));
+    }
+
+    @Test
+    void validaSorvetePremiumComCasquinhaAcimaDeUmaCobertura() {
+        Assertions.assertEquals(24,sorveteria.precoSorvete(true,true,2));
+        Assertions.assertEquals(24,sorveteria.precoSorvete(true,true,3));
     }
 }
